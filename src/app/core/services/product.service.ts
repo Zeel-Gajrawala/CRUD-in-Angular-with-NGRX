280b562189af9e0c64e42981d5e004c313a788de
ZeelGajrawala <zeel.gajrawala.work@gmail.com>
2023-06-03 23:22:55 +0530

    used json-server for apis. APi running on loaclhost:3000

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -7,21 +7,23 @@ import { Product } from 'src/app/views/product/interfaces/product.interface';
 })
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
+  apiUrl: string = 'http://localhost:3000';
 
-  getAllProducts(){
-    return this.http.get<Product[]>('https://dummyjson.com/products');
+  constructor(private http: HttpClient) { }
+
+  getAllProducts() {
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
   }
 
-  createProduct(product:Product){
-    return this.http.post('https://dummyjson.com/products/add',product);
+  createProduct(product: Product) {
+    return this.http.post(`${this.apiUrl}/products`, product);
   }
 
-  updateProduct(productId:number | string,product:Partial<Product>){
-    return this.http.put(`https://dummyjson.com/products/${productId}`,product);
+  updateProduct(productId: number | string, product: Partial<Product>) {
+    return this.http.patch(`${this.apiUrl}/products/${productId}`, product);
   }
 
-  deleteProduct(productId:number | string){
-    return this.http.delete(`https://dummyjson.com/products/${productId}`);
+  deleteProduct(productId: number | string) {
+    return this.http.delete(`${this.apiUrl}/products/${productId}`);
   }
 }
